facdbbf63771387d5fbfc54631c079b8308a8b78
asgorlov <asgorlov@outlook.com>
2020-08-21 12:45:14 +0300

    Mediator

---
+++ src/main/java/org/novaworld/behavioral/mediator/Chat.java
@@ -0,0 +1,5 @@
+package org.novaworld.behavioral.mediator;
+
+public interface Chat {
+    void sendMessage(String message, User user);
+}
---
+++ src/main/java/org/novaworld/behavioral/mediator/User.java
@@ -0,0 +1,7 @@
+package org.novaworld.behavioral.mediator;
+
+public interface User {
+    void sendMessage(String message);
+
+    void getMessage(String message);
+}
